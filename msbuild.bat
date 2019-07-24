a0404b61eb38a4161cb7c984a24fb38c07dc699c
oopstu <heath@decisions.com>
2019-07-24 17:14:41 -0400

    Updated thanks to help from @jhallam3 cleaned this up to be runnable assuming local install of Decisions to default paths.

--- msbuild.bat
+++ msbuild.bat
@@ -1,18 +1,14 @@
 echo off
 @echo Building with MsBuild
-@cd /d "%~dp0"
-IF Exist "%programfiles(x86)%\MSBuild\14.0" (
-	set MSBUILDVER=14.0
-) else (
-	set MSBUILDVER=12.0
-)
 
-echo Running: "%programfiles(x86)%\MSBuild\%MSBUILDVER%\bin\msbuild.exe"
-"%programfiles(x86)%\MSBuild\%MSBUILDVER%\bin\msbuild.exe" build.proj
+REM Set Path to MSBUILD.exe here
+
+echo Running: "%programfiles(x86)%\MSBuild\14.0\bin\msbuild.exe" build.proj
+"%programfiles(x86)%\MSBuild\14.0\bin\msbuild.exe" build.proj
 
 pause
 net stop "Service Host Manager Watcher"
 net stop "Service Host Manager"
-copy ..\..\output\modules\Decisions.AzureServices.zip "c:\Program Files\Decisions\Decisions Services Manager\modules" /Y
+copy .\Example.Module.zip "c:\Program Files\Decisions\Decisions Services Manager\modules" /Y
 net start "Service Host Manager"
 pause
